61466a3ef05f9944c71b70c6756d2bfb52a038be
DBN-cmd <65612229+DBN-cmd@users.noreply.github.com>
2020-12-16 16:36:57 -0330

    Coding for reverseing string that was entered by user

--- FirstReverse/Program.cs
+++ FirstReverse/Program.cs
@@ -8,8 +8,32 @@ namespace FirstReverse
 {
     class Program
     {
+        public static string FirstReverse(string str)
+        {
+
+            // code goes here  
+            int strCount = str.Count();
+            string revstr;
+            var sb = new StringBuilder();
+            int n = 0;
+
+            for (int i = strCount; i > 0; i--)
+            {
+                sb.Append(str[strCount - 1]);
+                n += 1;
+                strCount -= 1;
+
+            }
+            revstr = sb.ToString();
+            str = revstr;
+            return str;
+
+        }
+
         static void Main(string[] args)
         {
+            // keep this function call here
+            Console.WriteLine(FirstReverse(Console.ReadLine()));
         }
     }
 }
